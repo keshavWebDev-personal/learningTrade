a631bb2edb6cd97a586f5f912c9cee27330448a3
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-02-06 18:14:56 +0530

    Implenting a college selector page

---
+++ src/components/welcome/SearchableDropdown.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let colleges = []; 
+    
+
+</script>
+<div class="flex flex-col">
+    <label for="college">Select your College:</label>
+    <input type="text">
+    <select name="college" id="">
+        {#each colleges as item }
+            <option value="{item.name}">{item.name}</option>
+        {/each}
+    </select>
+</div>
+
+<style>
+    *{
+        display: flex;
+
+    }
+</style>
---
+++ src/components/welcome/welcome.svelte
@@ -0,0 +1,6 @@
+<script>
+    import SearchableDropdown from "./SearchableDropdown.svelte";
+    import colleges from "./collegelist.json"
+</script>
+
+<SearchableDropdown {colleges} />
