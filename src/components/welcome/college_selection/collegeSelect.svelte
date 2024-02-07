99329405ca2ddf57e2b11f93c58d739627d1a625
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-02-07 17:12:58 +0530

    Improved the search logic

--- src/components/welcome/college_selection/collegeSelect.svelte
+++ src/components/welcome/college_selection/collegeSelect.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { string } from "astro/zod";
+
     export let colleges: College[] = [];
 
     interface College {
@@ -11,21 +13,32 @@
             country: string;
         };
     }
+
     let filteredColleges: College[] = [];
-    let srch_value = '';
-    function srchInputHandeler() {        
+    let srch_value = "";
+    function srchInputHandeler() {
         if (srch_value == "") {
             filteredColleges = colleges;
             return;
         }
-        filteredColleges = colleges.filter((clg)=>clg.name.toLowerCase().includes(srch_value.toLowerCase()))
+        filteredColleges = colleges.filter((clg) => {
+            const search = srch_value.toLowerCase();
+            return (
+                clg.name.toLowerCase().includes(search) ||
+                Object.values(clg.address).some((value) =>
+                    value.toLowerCase().includes(search)
+                )
+            );
+        });
     }
     function clear() {
-        srch_value = ''
-        srchInputHandeler()
+        srch_value = "";
+        srchInputHandeler();
     }
-    srchInputHandeler()
-    
+    srchInputHandeler();
+    function cptlFstL(string:string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
 </script>
 
 <div
@@ -69,24 +82,31 @@
                 />
                 <button on:click={clear}>
                     <img
-                    src="svg/cross.svg"
-                    alt=""
-                    srcset=""
-                    class=" w-5 mr-2"
-                    width="1.25rem"
-                    height="1.25rem"
-                />
+                        src="svg/cross.svg"
+                        alt=""
+                        srcset=""
+                        class=" w-5 mr-2"
+                        width="1.25rem"
+                        height="1.25rem"
+                    />
                 </button>
-                
             </div>
             <div
                 class="w-full h-full max-h-fit bg-gradient-to-r from-[#FFF7AD] to-[#FFA9F9] flex flex-col items-center mt-4 rounded-t-3xl font-[poppins]"
             >
                 {#each filteredColleges as item}
                     <button
-                        class="  focus:bg-green-200 border-2 border-white bg-gray-100 min-h-20 w-1/2 text-2xl flex items-center pl-6 rounded-3xl mt-6  hover:scale-[1.02] transform-gpu shadow hover:shadow-lg  transition duration-200 "
+                        class="  focus:bg-green-200 border-2 border-white bg-gray-100 min-h-20 w-1/2 text-2xl text-left pl-6 rounded-3xl mt-6 hover:scale-[1.02] transform-gpu shadow hover:shadow-lg transition duration-200"
                     >
                         {item.name}
+                        <br />
+                        <p class=" text-base">
+                            {cptlFstL(item.address.local)},
+                            {cptlFstL(item.address.city)},
+                            {cptlFstL(item.address.district)},
+                            {cptlFstL(item.address.state)},
+                            {cptlFstL(item.address.country)}
+                        </p>
                     </button>
                 {/each}
             </div>
