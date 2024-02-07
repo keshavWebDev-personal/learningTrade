bee0548fc58c5028328116664c4f72631a6211fd
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-02-07 08:46:23 +0530

    more updates

--- src/components/welcome/SearchableDropdown.svelte
+++ src/components/welcome/SearchableDropdown.svelte
@@ -2,7 +2,8 @@
     export let colleges = [];
 </script>
 
-<div class="h-screen w-screen flex flex-col items-center p-0 m-0 box-border">
+
+<div class="min-h-screen max-h-max w-full flex flex-col items-center p-0 m-0 box-border">
     
     <div class="flex flex-col  w-4/6  h-full">
         <div class=" w-full flex items-center mt-12 gap-4 ">
@@ -12,22 +13,18 @@
                 <span name="progrress-bar-completed" class=" w-1/6 bg-green-600 " ></span>
             </div>   
         </div>
-        <label for="" class="text-6xl mt-6 " >Tell us about your College</label>
+        <label for="" class=" font-sans text-6xl mt-6 " >Tell us about your College</label>
         <div class=" w-full h-full flex flex-col items-center mt-8" >
             <div class=" justify-between w-96 h-14 rounded-full b bg-gradient-to-r from-[#FFF7AD] to-[#FFA9F9] flex items-center p-3" name="search-container">
                 <div class=" rounded-full  bg-[#004AAD] w-10 p-2 text-white fill-white"><img src="svg/search-icon.svg" alt="" srcset=""></div>
                 <input type="text" class="bg-[#00000000] w-3/4 h-full outline-none " placeholder="Search..">
                 <img src="svg/cross.svg" alt="" srcset="" class=" w-5 mr-2" >
             </div>
-            <div class="w-full h-full bg-gradient-to-r from-[#FFF7AD] to-[#FFA9F9] flex flex-col items-center mt-4 rounded-t-3xl ">
+            <div class="w-full min-h-full max-h-fit bg-gradient-to-r from-[#FFF7AD] to-[#FFA9F9] flex flex-col items-center mt-4 rounded-t-3xl ">
                 {#each colleges as item}
-                    <div class=" border-2 border-white bg-gray-100  h-20 w-1/2 text-2xl flex items-center pl-6 rounded-3xl mt-6"  >{item.name}</div>
+                    <div class=" border-2 border-white bg-gray-100  min-h-20 w-1/2 text-2xl flex items-center pl-6 rounded-3xl mt-6"  >{item.name}</div>
                 {/each}
             </div>
         </div>
-
     </div>
 </div>
-<style>
-    
-</style>
