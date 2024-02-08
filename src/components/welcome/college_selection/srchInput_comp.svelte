cffd5e23113da1ca1eb0657e602eeee5980ac9eb
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-02-08 14:46:34 +0530

    Updates

---
+++ src/components/welcome/college_selection/srchInput_comp.svelte
@@ -0,0 +1,12 @@
+
+<script lang="ts">
+    export let srch_value:String;
+    export let srchInputHandeler:Function;
+</script>
+<input
+    type="text"
+    class="bg-[#00000000] w-3/4 h-full outline-none"
+    placeholder="Search.."
+    bind:value={srch_value}
+    on:input={() => srchInputHandeler()}
+/>
--- src/components/welcome/get_started.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    import CollegeSelect from "./college_selection/collegeSelect.svelte";
-    import colleges from "./college_selection/collegelist.json"
-</script>
-
-<CollegeSelect {colleges}/>
