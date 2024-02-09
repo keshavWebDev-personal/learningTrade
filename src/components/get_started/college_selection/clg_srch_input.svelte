50343bd61352dfa060e8b8b9fc74bc68feb5392b
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-02-09 21:51:26 +0530

    made the search input modular

--- src/components/get_started/college_selection/clg_srch_input.svelte
+++ src/components/get_started/college_selection/clg_srch_input.svelte
@@ -1,1 +1,43 @@
-<input type="text" class="bg-[#00000000] w-3/4 h-full outline-none" placeholder="Search..">
+<script lang="ts">
+    import collegelist from './store.ts'
+    import collegelistOg from '../../../data/collegelist.json'
+
+    type College = { 
+        name: string;
+        address: {
+            local: string;
+            city: string;
+            district: string;
+            state: string;
+            country: string;
+        };
+    }
+    let srch_value: string = "";
+    let filtered: College[] = []
+
+    function srchInputHandeler() {
+        if (srch_value == ''){
+            collegelist.set(collegelistOg)
+            return
+        }
+        collegelist.subscribe((data)=>{
+            filtered = data.filter(clg=>{
+                const search = srch_value.toLowerCase();
+                if (clg.name.toLowerCase().includes(search)) {
+                    return true
+                }
+            })
+        })
+        collegelist.set(filtered)
+    }
+    srchInputHandeler()
+
+</script>
+
+<input
+    type="text"
+    class="bg-[#00000000] w-3/4 h-full outline-none"
+    placeholder="Search.."
+    bind:value={srch_value}
+    on:input={() => srchInputHandeler()}
+/>
