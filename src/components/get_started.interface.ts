2e787b250cb4cdec062d54b604327a14e872abeb
keshav-writes-code <dhimankeshav201@gmail.com>
2024-02-15 15:49:59 +0530

    store types in a seperate file

---
+++ src/components/get_started.interface.ts
@@ -0,0 +1,11 @@
+type College = { 
+    name: string;
+    address: {
+        local: string;
+        city: string;
+        district: string;
+        state: string;
+        country: string;
+    };
+    id: string
+}
