61c326c90e3a6e547dfb5acedc42b29f9625d951
keshav-writes-code <dhimankeshav201@gmail.com>
2024-02-15 15:50:57 +0530

    made cleaer button functional

--- src/components/get_started/college_selection/clg_srch_input.svelte
+++ src/components/get_started/college_selection/clg_srch_input.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-    import collegelist from './store.ts'
-    import collegelistOg from '../../../data/collegelist.json'
+    import { collegelist, Mcolleges as collegelistOg } from "./store.ts";
 
-    type College = { 
+    type College = {
         name: string;
         address: {
             local: string;
@@ -11,33 +10,54 @@
             state: string;
             country: string;
         };
-    }
+    };
     let srch_value: string = "";
-    let filtered: College[] = []
+    let filtered: College[] = [];
 
     function srchInputHandeler() {
-        if (srch_value == ''){
-            collegelist.set(collegelistOg)
-            return
+        if (srch_value == "") {
+            collegelist.set(collegelistOg);
+            return;
         }
-        collegelist.subscribe((data)=>{
-            filtered = data.filter(clg=>{
+        collegelist.subscribe((data) => {
+            filtered = data.filter((clg) => {
                 const search = srch_value.toLowerCase();
                 if (clg.name.toLowerCase().includes(search)) {
-                    return true
+                    return true;
                 }
-            })
-        })
-        collegelist.set(filtered)
+            });
+        });
+        collegelist.set(filtered);
     }
-    srchInputHandeler()
+    srchInputHandeler();
 
+    function srchClear(){
+        collegelist.set(collegelistOg);
+            let input = document.getElementById("input") as HTMLInputElement|null 
+            if (!input) return
+            input.value = ''
+    }
 </script>
 
-<input
-    type="text"
-    class="bg-[#00000000] w-3/4 h-full outline-none"
-    placeholder="Search.."
-    bind:value={srch_value}
-    on:input={() => srchInputHandeler()}
-/>
+<div
+    class="lg:w-96 lg:h-14 lg:p-3 md:text-lg text-sm sm:w-80 sm:h-12 sm:p-2 w-2/3 h-10 p-1 rounded-full b bg-gradient-to-r from-[#FFF7AD] to-[#FFA9F9] flex items-center justify-between"
+>
+    <div
+        class=" rounded-full bg-[#004AAD] lg:w-10 lg:p-2 sm:w-8 sm:p-2 w-6 p-1 text-white fill-white"
+    >
+        <img src="svg/search-icon.svg" alt="" srcset="" />
+    </div>
+    <input
+        type="text"
+        id="input"
+        class="bg-[#00000000] w-3/4 h-full outline-none"
+        placeholder="Search.."
+        bind:value={srch_value}
+        on:input={() => srchInputHandeler()}
+    />
+    <button
+        on:click={() => srchClear()}
+    >
+        <img src="svg/cross.svg" alt="" srcset="" class=" sm:w-5 w-3 mr-2" />
+    </button>
+</div>
--- src/components/get_started/college_selection/store.ts
+++ src/components/get_started/college_selection/store.ts
@@ -1,6 +1,15 @@
 import { writable } from 'svelte/store';
 import colleges from '../../../data/collegelist.json';
+import { v4 as uuidv4 } from 'uuid';
+import '../../get_started.interface.ts'
 
-const collegelist = writable(colleges); // Initial color
+let Mcolleges = colleges.map((college, index)=>{
+    return {
+        id: uuidv4(),
+        ...college
+    }
+})
 
-export default collegelist;
+const collegelist = writable(Mcolleges);
+
+export {collegelist, Mcolleges};
