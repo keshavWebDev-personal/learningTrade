c666adbe51c5d5b57c9308bc0c3ec049806ec5a1
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-02-15 22:06:26 +0530

    made alias in a seperate section of the button

--- src/components/get_started/college_selection/clg_btn_spawner.svelte
+++ src/components/get_started/college_selection/clg_btn_spawner.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import {collegelist} from './store.ts'
-    import '../../get_started.interface.ts'
+    import type { College } from '../../get_started.interface.ts'
     
     function cptlFstL(string:string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
@@ -14,27 +14,30 @@
             return item
         })
     })
-
-
 </script>
 
 <div
-    class=" pb-5 w-full h-full max-h-fit bg-gradient-to-r from-[#FFF7AD] to-[#FFA9F9] flex flex-col items-center mt-4 rounded-t-3xl font-[poppins] dark:bg-gradient-to-r from-[#635c24] to-[#6d2268] "
+    class=" pb-5 w-full h-full max-h-fit bg-gradient-to-r from-[#FFF7AD] to-[#FFA9F9] flex flex-col items-center mt-4 rounded-t-3xl font-[poppins] dark:bg-gradient-to-r "
 >
     {#each buttons as item}
     <button
-        class=" active:scale-[0.99] active:bg-gray-200 focus:bg-green-200 focus:shadow-lg focus:scale-[1.02] border-2 border-white bg-gray-100 sm:min-h-20 min-h-16 md:w-1/2 sm:w-3/4 w-11/12  xl:text-xl lg:text-lg md:text-base sm:text-sm text-xs text-left sm:px-6 sm:py-3 px-3 py-1 rounded-3xl mt-6 hover:scale-[1.02] transform-gpu shadow hover:shadow-lg hover:bg-gray-50 transition duration-200"
+        class=" active:scale-[0.99] active:bg-gray-200 focus:bg-green-200 focus:shadow-lg focus:scale-[1.02] border-2 border-white bg-gray-100 sm:min-h-20 min-h-16 md:w-1/2 sm:w-3/4 w-11/12  xl:text-xl lg:text-lg md:text-base sm:text-sm text-xs text-left sm:px-6 sm:py-3 px-3 py-1 rounded-3xl mt-6 hover:scale-[1.02] transform-gpu shadow hover:shadow-lg hover:bg-gray-50 transition duration-200 flex items-center justify-between"
         id={item.id}
     >
-        {item.name}
-        <br />
-        <p class=" xl:text-base lg:text-sm md:text-xs sm:text-[0.7rem] text-[0.5rem] ">
-            {cptlFstL(item.address.local)},
-            {cptlFstL(item.address.city)},
-            {cptlFstL(item.address.district)},
-            {cptlFstL(item.address.state)},
-            {cptlFstL(item.address.country)}
-        </p>
+        <span class="">
+            <p >{item.name}</p>
+
+            <p class="font-[SourceCodePro] xl:text-base lg:text-sm md:text-xs sm:text-[0.7rem] text-[0.5rem] ">
+                {cptlFstL(item.address.local)},
+                {cptlFstL(item.address.city)},
+                {cptlFstL(item.address.district)},
+                {cptlFstL(item.address.state)},
+                {cptlFstL(item.address.country)}
+            </p>
+        </span>
+
+        <p class="font-[BebasNeue] xl:text-2xl">{item.alias}</p>
+
     </button>
     {/each}
 </div>
