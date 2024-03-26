e672f2623db63249ead8e1b31b505fb3c3993c50
keshav-writes-code <dhimankeshav201@gmail.com>
2024-03-26 16:12:07 +0530

    Someone Please FIx my code

--- src/components/get_started/1_college_selection/clg_btn_spawner.svelte
+++ src/components/get_started/1_college_selection/clg_btn_spawner.svelte
@@ -18,6 +18,7 @@
 <div class="w-full grid place-items-center select-none">
     {#each buttons as item}
         <button
+        
             class=" active:scale-[0.99] active:bg-gray-200 focus:bg-green-200 focus:shadow-lg focus:scale-[1.02] border-2 border-white bg-gray-100 sm:min-h-20 min-h-16 md:w-1/2 sm:w-3/4 w-11/12 xl:text-xl lg:text-lg md:text-base sm:text-sm text-xs text-left sm:px-6 sm:py-5 px-4 py-1 rounded-3xl mt-6 hover:scale-[1.02] transform-gpu shadow hover:shadow-lg hover:bg-gray-50 transition duration-200 flex items-center justify-between dark:bg-neutral-700 dark:border dark:border-[#747474] dark:hover:bg-neutral-600 dark:focus:bg-[#415543]"
             id={item.id}
             aria-label="trig_button"
