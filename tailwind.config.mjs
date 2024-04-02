e9007efcaebed0a42f6d2e356d618d2b2b5a1642
keshav-writes-code <dhimankehsav201@gmail.com>
2024-04-02 06:45:21 +0530

    Fresh Start

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,20 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    theme: {
-        fonts: {
-            sans: ["HelveticaWorld", "sans-serif"],
-            poppins: ["Poppins", "sans-serif"],
-            SourceCodePro: ["SourceCodePro", "sans-serif"],
-            BebasNeue: ["BebasNeue", "sans-serif"],
-        },
-    },
-    extend: {
-        transitionTimingFunction: {
-            "in-expo": "cubic-bezier(0.95, 0.05, 0.795, 0.035)",
-            "out-expo": "cubic-bezier(0.19, 1, 0.22, 1)",
-        },
-    },
-
-    plugins: [require("daisyui")],
-};
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("daisyui")],
+}
