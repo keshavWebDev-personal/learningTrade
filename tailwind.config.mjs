1e84e92ddf60b33204abda6bf6bb19edf63c3e1c
keshav-writes-code <dhimankehsav201@gmail.com>
2024-04-05 23:24:37 +0530

    Working onthe UI

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [require("daisyui")],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {},
+    },
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: "#ce00ea",
+
+                    secondary: "#7e6600",
+
+                    accent: "#007500",
+
+                    neutral: "#141414",
+
+                    "base-100": "#1f253a",
+
+                    info: "#007ff4",
+
+                    success: "#65a300",
+
+                    warning: "#bc8700",
+
+                    error: "#ff8a95",
+                },
+            },
+        ],
+    },
+    plugins: [require("daisyui")],
+};
